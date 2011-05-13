802926bd82fab04b94f7b29c8e7b0348d8d01312
Brandyn A. White <bwhite@dappervision.com>
2011-05-13 18:48:35 -0400

    Hid all features that use ctypes

--- imfeat/__init__.py
+++ imfeat/__init__.py
@@ -4,10 +4,10 @@ import rand
 #from _histogram_joint import histogram_joint
 from _imfeat_autocorrelogram import Autocorrelogram
 from _imfeat_histogram import Histogram
-from _rhog import rhog, rhog_rgb
-from _rhog_dalal import rhog as rhog_dalal
-from _surf import surf_feature as surf
-from _surf import surf_random
+#from _rhog import rhog, rhog_rgb
+#from _rhog_dalal import rhog as rhog_dalal
+#from _surf import surf_feature as surf
+#from _surf import surf_random
 from _bovw import BoVW
 import filter_bank
 from _faces import faces
