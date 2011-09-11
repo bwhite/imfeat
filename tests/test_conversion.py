d08b62bb0c9d2c9a7d251071402a6bda9b0334e5
Brandyn A. White <bwhite@dappervision.com>
2011-09-11 03:37:38 -0400

    Cleaned up, removed old conversions Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -24,12 +24,13 @@ class Test(unittest.TestCase):
         to_np8 = lambda x: imfeat.convert_image(x, {'type': 'numpy', 'dtype': 'uint8', 'mode': 'bgr'})
         to_np32 = lambda x: imfeat.convert_image(x, {'type': 'numpy', 'dtype': 'float32', 'mode': 'bgr'})
         for fn in ['lena.jpg', 'lena.pgm', 'lena.ppm']:
+            image_np8 = None
+            image_np32 = None
             for i in load_images(fn):
-                image_np8 = to_np8(i)
-                image_np32 = to_np32(i)
-                np.testing.assert_equal(image_np8, np.array(image_np32 * 255, dtype=np.uint8))
-                image_np8 = imfeat.convert_image(i, {'type': 'numpy', 'dtype': 'uint8', 'mode': 'bgr'})
-                image_np32 = imfeat.convert_image(i, {'type': 'numpy', 'dtype': 'float32', 'mode': 'bgr'})
+                if image_np8 is None:
+                    image_np8 = to_np8(i)
+                    image_np32 = to_np32(i)
+                    np.testing.assert_equal(image_np8, np.array(image_np32 * 255, dtype=np.uint8))
                 for t, c, m in type_channel_modes:
                     cur_img = imfeat.convert_image(i, {'type': t, 'dtype': m, 'mode': c})
                     if t == 'opencv':
@@ -49,7 +50,6 @@ class Test(unittest.TestCase):
         for fn in ['lena.jpg', 'lena.pgm', 'lena.ppm']:
             for i in load_images(fn):
                 for h, w in [(50, 50), (100, 50), (50, 100), (1000, 100), (100, 1000)]:
-                    print((fn, type(i)))
                     out_arr = to_np8(imfeat.resize_image(i, h, w))
                     self.assertEqual(out_arr.shape, (h, w, 3))
                     #cv2.imwrite('resize-out-%.3d.jpg' % n, out_arr)
