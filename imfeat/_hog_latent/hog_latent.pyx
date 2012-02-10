ad00c71e582b1f32a4323c100a7e264881261aea
Brandyn A. White <bwhite@dappervision.com>
2012-02-10 12:06:07 -0500

    Updated hog to use 31 instead of 32 dims and blocks=2
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- imfeat/_hog_latent/hog_latent.pyx
+++ imfeat/_hog_latent/hog_latent.pyx
@@ -16,7 +16,7 @@ cdef class HOGLatent(imfeat.BaseFeature):
     cdef int _sbin
     cdef object _blocks
 
-    def __init__(self, sbin=2, blocks=1):
+    def __init__(self, sbin=2, blocks=2):
         super(HOGLatent, self).__init__()
         self.MODES = [{'type': 'numpy', 'dtype': 'uint8', 'mode': 'rgb'}]
         self._sbin = sbin
@@ -29,7 +29,7 @@ cdef class HOGLatent(imfeat.BaseFeature):
         cdef np.ndarray out = np.zeros(feat_shape[::-1], dtype=np.float64)
         process(<np.float64_t *>image.data, image.shape[0], image.shape[1], self._sbin, <np.float64_t *>out.data, np.prod(feat_shape))
         out = out.T
-        #out = out[:, :, :-1]
+        out = np.asfarray(out[:, :, :-1])
         xcoords, ycoords = np.meshgrid(range(image_input.shape[1]), range(image_input.shape[0]))
         xcoords = np.clip(xcoords / self._sbin, 0, out.shape[1] - 1)
         ycoords = np.clip(ycoords / self._sbin, 0, out.shape[0] - 1)
@@ -61,7 +61,7 @@ cdef class HOGLatent(imfeat.BaseFeature):
         cdef np.ndarray out = np.zeros(feat_shape[::-1], dtype=np.float64)
         process(<np.float64_t *>image.data, image.shape[0], image.shape[1], sbin, <np.float64_t *>out.data, np.prod(feat_shape))
         out = out.T
-        #out = out[:, :, :-1]
+        out = np.asfarray(out[:, :, :-1])
         out = self._make_blocks(out, blocks)
         return np.ascontiguousarray(out.reshape((out.shape[0] * out.shape[1], out.shape[2])))
 
@@ -95,7 +95,7 @@ cdef class HOGLatent(imfeat.BaseFeature):
         cdef np.ndarray out = np.zeros(feat_shape[::-1], dtype=np.float64)
         process(<np.float64_t *>image.data, image.shape[0], image.shape[1], sbin, <np.float64_t *>out.data, np.prod(feat_shape))
         out = out.T
-        #out = out[:, :, :-1]
+        out = np.asfarray(out[:, :, :-1])
         out = self._make_blocks(out, blocks)
         bow_shape = out.shape[0], out.shape[1]
         out = out.reshape((out.shape[0] * out.shape[1], out.shape[2]))
@@ -117,6 +117,6 @@ cdef class HOGLatent(imfeat.BaseFeature):
         cdef np.ndarray out = np.zeros(feat_shape[::-1], dtype=np.float64)
         process(<np.float64_t *>image.data, image.shape[0], image.shape[1], self._sbin, <np.float64_t *>out.data, np.prod(feat_shape))
         out = out.T
-        #out = out[:, :, :-1]
+        out = np.asfarray(out[:, :, :-1])
         out = self._make_blocks(out, self._blocks)
         return [np.ascontiguousarray(out.ravel())]
