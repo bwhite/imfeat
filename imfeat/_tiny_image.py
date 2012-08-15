0008b1f067ea376cf4c8142890819642081d6d2f
Brandyn A. White <bwhite@dappervision.com>
2012-08-15 10:30:07 -0400

    Added image mode to tiny_image
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- imfeat/_tiny_image.py
+++ imfeat/_tiny_image.py
@@ -4,11 +4,11 @@ import numpy as np
 
 class TinyImage(imfeat.BaseFeature):
 
-    def __init__(self, size=32):
-        super(TinyImage, self).__init__({'type': 'numpy', 'dtype': 'uint8', 'mode': 'bgr'})
+    def __init__(self, size=32, mode='bgr'):
+        self.mode = mode
+        super(TinyImage, self).__init__({'type': 'numpy', 'dtype': 'uint8', 'mode': mode})
         self.size = size
 
     def __call__(self, image):
         image = self.convert(image)
-        return np.asfarray(imfeat.resize_image(image, self.size, self.size,
-                                               {'type': 'numpy', 'dtype': 'uint8', 'mode': 'bgr'}).ravel()) / 255.
+        return np.asfarray(imfeat.resize_image(image, self.size, self.size).ravel()) / 255.
