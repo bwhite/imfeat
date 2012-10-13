af67b658d24c6f334c92d294689f176bdef96cf1
Brandyn A. White <bwhite@dappervision.com>
2012-10-13 12:32:17 -0400

    Moved face detection as a member function
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- imfeat/_faces/faces.py
+++ imfeat/_faces/faces.py
@@ -5,21 +5,6 @@ import imfeat
 from . import __path__
 
 
-def _detect_faces(img, cascade):
-    min_size = (20, 20)
-    image_scale = 2
-    haar_scale = 1.2
-    min_neighbors = 2
-    haar_flags = 0
-    small_width = img.shape[1] / image_scale
-    small_height = img.shape[0] / image_scale
-    small_img = cv2.resize(img, (small_width, small_height))
-    cv2.equalizeHist(small_img, small_img)
-    faces = cascade.detectMultiScale(small_img, scaleFactor=haar_scale, minNeighbors=min_neighbors, flags=haar_flags, minSize=min_size)
-    return [((x * image_scale, y * image_scale,
-              w * image_scale, h * image_scale), n)
-            for (x, y, w, h), n in faces]
-
 
 class Faces(imfeat.BaseFeature):
 
@@ -35,9 +20,25 @@ class Faces(imfeat.BaseFeature):
             else:
                 raise ValueError("Can't find .xml file!")
 
+    def _detect_faces(self, img):
+        cascade = self._cascade
+        min_size = (20, 20)
+        image_scale = 2
+        haar_scale = 1.2
+        min_neighbors = 2
+        haar_flags = 0
+        small_width = img.shape[1] / image_scale
+        small_height = img.shape[0] / image_scale
+        small_img = cv2.resize(img, (small_width, small_height))
+        cv2.equalizeHist(small_img, small_img)
+        faces = cascade.detectMultiScale(small_img, scaleFactor=haar_scale, minNeighbors=min_neighbors, flags=haar_flags, minSize=min_size)
+        return [((x * image_scale, y * image_scale,
+                  w * image_scale, h * image_scale), n)
+                for (x, y, w, h), n in faces]
+
     def __call__(self, image):
         image = self.convert(image)
-        faces = _detect_faces(image, self._cascade)
+        faces = self._detect_faces(image)
         if faces:
             mean_pos = np.mean(np.asfarray([x[0] for x in faces]), 0)
             mean_pos[0] /= image.size[0]
